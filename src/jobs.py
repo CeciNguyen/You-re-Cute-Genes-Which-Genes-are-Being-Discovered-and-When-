e3b39371205e31e70e9ffa4e45d22f52aa43414d
Ubuntu <ubuntu@avlav-vm.js2local>
2023-04-27 02:55:27 +0000

    updated jobs

--- src/jobs.py
+++ src/jobs.py
@@ -3,7 +3,7 @@ import os
 from hotqueue import HotQueue
 import redis
 
-redis_ip = os.environ.get('REDIS_IP', '172.17.0.1')
+redis_ip = os.environ.get('REDIS_IP')
 
 q = HotQueue("queue", host=redis_ip, port=6379, db=3)
 rd = redis.Redis(host=redis_ip, port=6379, db=4)
